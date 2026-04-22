425a83c5bc6d0513d68a9e6d0f0584a256e61adf
Riley Beck <beckriley515@gmail.com>
2026-04-22 10:19:58 -0600

    Added some keymaps for lsp features. Added VimBeGood by primeagen

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -4,9 +4,14 @@
 vim.g.mapleader = " " --Spacebar leader key
 vim.keymap.set("n","-", vim.cmd.Ex) -- in normal mode - will open file explorer
 vim.keymap.set("n", "<leader>ft", "<cmd>split | terminal<CR>", { desc = "Open terminal" })
-
+vim.keymap.set("i", "kj", "<esc>")
+vim.keymap.set("v", "kj", "<esc>")
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
 --generate docstring
 vim.keymap.set("n", "<leader>ds", function()
   require("neogen").generate()
 end)
+
+--LSP signature
+vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help)
